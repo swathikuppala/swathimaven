9b2e103c0178614e68000ca74e0dae18307b14bf
swathikuppala <kswathi515@gmail.com>
2022-05-11 23:27:54 +0530

    second

---
+++ swathi/src/main/java/swathi/Helloworld.java
@@ -0,0 +1,11 @@
+package swathi;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to maven");
+
+	}
+
+}
